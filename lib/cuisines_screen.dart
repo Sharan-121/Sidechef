bf89e59bd0afbf46e067f80836543ecef65de212
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-02-15 10:54:07 +0530

    Added grid view with card

---
+++ lib/cuisines_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './cuisine_data.dart';
+import './cuisine_item.dart';
+
+class CuisineScreen extends StatelessWidget {
+  const CuisineScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sidechef"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: <Widget>[
+          ...cuisineData
+              .map(
+                (data) => CuisineItem(title: data.title, color: data.color),
+              )
+              .toList(),
+        ],
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
