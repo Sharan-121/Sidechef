263a1d253c48d23f831111ef7f4cd8764fa260b6
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-02-15 10:08:49 +0530

    Added Cuisine data and model

---
+++ lib/cuisine_data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import './models/cuisine.dart';
+
+const cuisineData = [
+  Cuisine(id: "1", title: "Italian", color: Colors.purple),
+  Cuisine(id: "2", title: "Quick & Easy", color: Colors.red),
+  Cuisine(id: "3", title: "Hamburgers", color: Colors.orange),
+  Cuisine(id: "4", title: "German", color: Colors.amber),
+  Cuisine(id: "5", title: "Light & Lovely", color: Colors.blue),
+  Cuisine(id: "6", title: "Exotic", color: Colors.green),
+  Cuisine(id: "7", title: "Breakfast", color: Colors.lightBlue),
+  Cuisine(id: "8", title: "Asian", color: Colors.lightGreen),
+  Cuisine(id: "9", title: "French", color: Colors.pink),
+  Cuisine(id: "10", title: "Summer", color: Colors.teal),
+];
